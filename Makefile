0a460423cecc487edff97e48089150919580c920
Nathaniel D'Rozario <nsdrozario@gmail.com>
2020-07-29 14:57:20 -0700

    fixed compile errors

--- Makefile
+++ Makefile
@@ -4,12 +4,12 @@ CXX = g++
 CXXFLAGS = -Iinclude/ -Wall -Werror
 
 %.o: %.cpp include/l33tsp34k.hpp
-  $(CXX) $< -c -o $@ $(CXXFLAGS)
+	$(CXX) $< -c -o $@ $(CXXFLAGS)
  
 l33tsp34k: l33tsp34k.o l33tsp34k_aux.o
-  $(CXX) l33tsp34k.o l33tsp34k_aux.o -o l33tsp34k
- 
+	$(CXX) l33tsp34k.o l33tsp34k_aux.o -o l33tsp34k
+
 clean:
-  rm -rf %.o
-  rm -rf l33tsp34k
-  rm -rf l33tsp34k.exe
+	rm -rf %.o
+	rm -rf l33tsp34k
+	rm -rf l33tsp34k.exe
--- l33tsp34k.cpp
+++ l33tsp34k.cpp
@@ -1,6 +1,7 @@
 // l33tsp34k translator (C) 2020 Nathaniel D'Rozario | see LICENSE file for license
 #include <iostream>
 #include <fstream>
+#include <cstring>
 #include <l33tsp34k.hpp>
 
 std::ifstream file_in;
@@ -10,7 +11,7 @@ char tmp;
 int main (int argc, char *argv[]) {
 
     if (argc != 4) {
-        std::cout << 'usage: l33tsp34k [-l | -p] file_in file_out\n-l for converting to leetspeak\n-p for converting to plaintext' << std::endl;
+        std::cout << "usage: l33tsp34k [-l | -p] file_in file_out\n-l for converting to leetspeak\n-p for converting to plaintext" << std::endl;
     }
 
     file_in.open(argv[2]);
@@ -22,11 +23,11 @@ int main (int argc, char *argv[]) {
 
     file_out.open(argv[3]);
 
-    if (argv[1] == "-l") {
+    if (std::strcmp(argv[1], "-l")) {
         while (file_in >> tmp) {
             file_out << l33tsp34k::conv_to_leetspeak(tmp);
         }
-    } else if (argv[1] == "-p") {
+    } else if (std::strcmp(argv[1], "-p")) {
         while (file_in >> tmp) {
             file_out << l33tsp34k::conv_to_plaintext(tmp);
         }
